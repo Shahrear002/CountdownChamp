87d7104e92cb3b63a0a23befced8a0d0a2ed4f64
Shahrear002 <shabuz2900@gmail.com>
2018-07-30 03:22:48 +0600

    Countdownchamp Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,39 @@
 import React, { Component } from 'react';
+import Clock from './Clock';
+import './App.css';
+import {Form, FormControl, Button } from 'react-bootstrap';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deadLine: 'December 25, 2018',
+      newDeadLine: ''
+    }
+  }
+
+  changeDeadLine() {
+    this.setState({deadLine: this.state.newDeadLine});
+  }
+
   render() {
     return (
-      <div>Countdown Champ, App Component</div>
+      <div className="App">
+        <div className="App-title" >
+          Countdown to {this.state.deadLine}
+        </div>
+        <Clock
+          deadLine={this.state.deadLine}
+        />
+        <Form inline>
+          <FormControl className='DeadLine-input'
+            placeholder='new date'
+            onChange={event => this.setState({newDeadLine: event.target.value})}
+          />
+          <Button onClick={() => this.changeDeadLine()}>Submit</Button>
+        </Form>
+
+      </div>
     )
   }
 }
